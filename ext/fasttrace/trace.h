8d73499af1ada84d3814caf44778f0aa146fa785
Nigel Baillie <metreckk@gmail.com>
2022-02-25 15:40:04 +0900

    remove resolve_klass for now. it caused trouble

--- ext/fasttrace/trace.h
+++ ext/fasttrace/trace.h
@@ -19,6 +19,7 @@ enum {
     kObjectSingleton = 0x8, // Singleton of an object
     kOtherSingleton = 0x10  // Singleton of unkown object
 };
+extern const unsigned int kSingleton;
 
 void ft_init_trace(void);
 
