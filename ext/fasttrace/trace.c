81af6988fa8c8b1587f60b786f4b7f0cc88e9036
Nigel Baillie <metreckk@gmail.com>
2022-02-25 12:07:24 +0900

    Add c data

--- ext/fasttrace/trace.c
+++ ext/fasttrace/trace.c
@@ -2,14 +2,85 @@
 
 VALUE cTrace;
 
+
+/*
+ * ==============================
+ * Custom allocation / GC support
+ * ==============================
+ *
+ * Just like much of this whole file, this is largely pulled from ruby-prof
+ * https://github.com/ruby-prof/ruby-prof/blob/master/ext/ruby_prof/rp_profile.c
+ *
+ * This stuff is necessary in order for GC to work properly when we hold
+ * references to Ruby objects in C code.
+ */
+
+static void trace_mark(void *data) {
+    trace_t *trace = (trace_t*)data;
+    rb_gc_mark(trace->tracepoint);
+}
+
+static void trace_ruby_gc_free(void *data) {
+    trace_t *trace = (trace_t*)data;
+    /*
+     * TODO: free any dynamically allocated sub-objects this might have.
+     */
+    xfree(trace);
+}
+
+size_t trace_size(const void* _data) {
+    return sizeof(trace_t);
+}
+
+static const rb_data_type_t trace_type =
+{
+    .wrap_struct_name = "Trace",
+    .function =
+    {
+        .dmark = trace_mark,
+        .dfree = trace_ruby_gc_free,
+        .dsize = trace_size,
+    },
+    .data = NULL,
+    .flags = RUBY_TYPED_FREE_IMMEDIATELY
+};
+
+static VALUE trace_allocate(VALUE klass) {
+    VALUE result;
+    trace_t* trace;
+
+    result = TypedData_Make_Struct(klass, trace_t, &trace_type, trace);
+    trace->tracepoint = Qnil;
+    trace->running = 0;
+
+    return result;
+}
+
+
+/*
+ * =====================
+ * Exported Ruby methods
+ * =====================
+ */
+
 static VALUE trace_start(VALUE self, VALUE trace_file_name) {
     const char *trace_file_name_cstr = StringValuePtr(trace_file_name);
+    trace_t *trace = RTYPEDDATA_DATA(self);
+
     printf("Hohoho %s\n", trace_file_name_cstr);
-    return Qnil;
+    return trace->tracepoint;
 }
+
+
+/*
+ * ================
+ * Class definition
+ * ================
+ */
 
 void ft_init_trace(void) {
     cTrace = rb_define_class_under(mFasttrace, "Trace", rb_cObject);
+    rb_define_alloc_func(cTrace, trace_allocate);
 
     rb_define_method(cTrace, "start", trace_start, 1);
 }
--- ext/fasttrace/trace.h
+++ ext/fasttrace/trace.h
@@ -3,6 +3,11 @@
 
 #include "fasttrace.h"
 
+typedef struct trace_t {
+    VALUE tracepoint;
+    int running;
+} trace_t;
+
 void ft_init_trace(void);
 
 #endif /* FASTTRACE_TRACE_H */
