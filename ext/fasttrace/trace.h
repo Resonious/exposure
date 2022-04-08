8d1d97308cd5d859aa86a5c585244a2948f852c5
Nigel Baillie <nigelbaillie@hey.com>
2022-04-08 17:16:15 +0900

    Cut more unused stuff

--- ext/fasttrace/trace.h
+++ ext/fasttrace/trace.h
@@ -23,15 +23,6 @@ typedef struct trace_t {
     int running;
 } trace_t;
 
-enum {
-    kModuleIncludee = 0x1,  // Included in module
-    kClassSingleton = 0x2,  // Singleton of a class
-    kModuleSingleton = 0x4, // Singleton of a module
-    kObjectSingleton = 0x8, // Singleton of an object
-    kOtherSingleton = 0x10  // Singleton of unkown object
-};
-extern const unsigned int kSingleton;
-
 void ft_init_trace(void);
 
 #endif /* FASTTRACE_TRACE_H */
